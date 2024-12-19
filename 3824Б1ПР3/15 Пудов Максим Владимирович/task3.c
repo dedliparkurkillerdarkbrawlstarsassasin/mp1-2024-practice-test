a0ac871cc4badca47eb4f8884daaad2ddfa46a73
pudov maxim <maksimpudov6@gmail.com>
2024-12-19 10:28:43 +0300

    Update task3.c

--- 3824Б1ПР3/15 Пудов Максим Владимирович/task3.c
+++ 3824Б1ПР3/15 Пудов Максим Владимирович/task3.c
@@ -1,7 +1,21 @@
 // Задача 3. Даны: символ ch и строка s. 
 // Сформировать новую строку, из которой удалены все вхождения символа ch. 
 // Пример: ch = ‘k’, s = “kabckdgkefgk”, ответ: “abcdgefg”.
-char* task3(char *s, char ch)
-{
-  return "task3";
+#include <stdlib.h>
+#include <string.h>
+
+char* task3(char* s, char ch){
+    int len = strlen(s), j = 0;
+    char* new = (char*)malloc(len + 1); 
+    if (new == NULL) {
+        return NULL; 
+    }
+    for (int i = 0; i < len; i++) {
+        if (s[i] != ch) {
+            new[j] = s[i];
+            j++;
+        }
+    }
+    new[j] = '\0'; 
+    return new;
 }
