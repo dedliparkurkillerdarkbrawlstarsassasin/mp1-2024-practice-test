ae992df0b7f64034bd596db257b6893d50cd3a78
pudov maxim <maksimpudov6@gmail.com>
2024-12-19 10:25:47 +0300

    Update task1.c

--- 3824Б1ПР3/15 Пудов Максим Владимирович/task1.c
+++ 3824Б1ПР3/15 Пудов Максим Владимирович/task1.c
@@ -1,7 +1,25 @@
 // Задача 1. Дан непустой массив A из N элементов.
 // Найти количество элементов, расположенных между первым минимальным
 // и последним максимальным элементами.
+#include <stdlib.h>
+
+
 int task1(unsigned A[], size_t N)
 {
-  return -1;
+if (N == 0) return -1;
+int min_ind = 0, max_ind = 0;
+unsigned min = A[0], max = A[0];
+for (size_t i = 1; i < N; ++i) {
+    if (A[i] < min) {
+        min = A[i];
+        min_ind = i;
+    }
+    if (A[i] >= max) {
+        max = A[i];
+        max_ind = i;
+    }
+}
+if (min_ind == max_ind) return -1;
+int count = abs(max_ind - min_ind) - 1;
+return count;
 }
